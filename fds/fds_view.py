6f7cdbebdb2768c08fa769e38d8a3f6563be6741
Jon Herbst <herbtaculargames@gmail.com>
2020-09-17 18:55:32 +0100

    Adding placeholder dropdown and button for waveform generation

--- fds/fds_view.py
+++ fds/fds_view.py
@@ -8,6 +8,22 @@ class FDSView(tk.Frame):
         self.createWidgets()
 
     def createWidgets(self):
+        types = ['Any']
+
+        self.wave_type = tk.StringVar(self.master)
+        self.wave_type.set('Any')
+
         self.label_title = tk.Label(self)
         self.label_title['text'] = 'FDS Generator'
         self.label_title.grid(row=0, column=0)
+
+        self.label_dropdown = tk.Label(self)
+        self.label_dropdown['text'] = 'Wave type: '
+        self.label_dropdown.grid(row=1, column=0)
+
+        self.dropdown_type = tk.OptionMenu(self, self.wave_type, *types)
+        self.dropdown_type.grid(row=1, column=1)
+
+        self.button_generate = tk.Button(self)
+        self.button_generate['text'] = 'Generate'
+        self.button_generate.grid(row=2, column=0)
