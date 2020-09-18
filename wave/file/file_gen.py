373c9b800aecc576de517a3942d60dfcd0ce6760
Jon Herbst <herbtaculargames@gmail.com>
2020-09-18 12:58:32 +0100

    Add missing text file generation class

---
+++ wave/file/file_gen.py
@@ -0,0 +1,26 @@
+import constants
+
+class FileGeneration:
+    def __init__(self, expansion):
+        self.file = ''
+
+    def generate(self):
+        self.file += self.headers()
+
+        return self.file
+        
+    def headers(self):
+        headers = ''
+        headers += (constants.WRAPPER.START() + '\n\n')
+        headers += (constants.SONG_INFORMATION.HEADER() + '\n')
+        headers += (constants.SONG_INFORMATION.TITLE() + '\n')
+        headers += (constants.SONG_INFORMATION.AUTHOR() + '\n')
+        headers += (constants.SONG_INFORMATION.COPYRIGHT() + '\n')
+
+        return headers
+
+    def instruments(self):
+        return
+
+    def footers(self):
+        return
