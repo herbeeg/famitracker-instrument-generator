386cca6b4678c2b47ad9a6359310a4206b0e9405
Jon Herbst <herbtaculargames@gmail.com>
2020-09-24 20:07:43 +0100

    Fixing wave wrapping values by snapping them to the lower/higher limits and documentation

--- wave/generators/fds/sine.py
+++ wave/generators/fds/sine.py
@@ -22,6 +22,21 @@ class FDSSineWaveGenerator(FDSWaveGenerator):
         super().__init__()
 
     def nextPair(self):
+        """
+        Generate a valid pair of waveform values that
+        can be included in the text export and
+        used to plot on the matplotlib
+        graph.
+
+        The variance is used to judge how far we can
+        stray from a standard sine wave but the
+        values cannot be wrapped around from 
+        64 -> 0 as this will completely
+        change the wave dynamics.
+
+        Returns:
+            tuple: Pair of FDS wave values
+        """
         pair_one = 0
         pair_two = 0
 
@@ -34,14 +49,13 @@ class FDSSineWaveGenerator(FDSWaveGenerator):
             self.wrapWaveValue(self.baseRepresentation()[self.wave_position+1] - self.variance),
             self.wrapWaveValue(self.baseRepresentation()[self.wave_position+1] + self.variance)
         ]
-
-        print(wave_range_first)
-        print(wave_range_second)
-
-        pair_one = random.randrange(wave_range_first[0], wave_range_first[1] + 1)
+            
+        pair_one = random.randrange(wave_range_first[0], wave_range_first[0] + 1)
         pair_two = random.randrange(wave_range_second[0], wave_range_second[1] + 1)
+        """Adding one to the range to include the ceiling value."""
 
         self.wave_position += 2
+        """Increment by two as we are returning a tuple instead of a single value."""
 
         return (pair_one, pair_two)
 
@@ -62,9 +76,26 @@ class FDSSineWaveGenerator(FDSWaveGenerator):
         ]
 
     def wrapWaveValue(self, wave_value):
+        """
+        If the wave value variation goes outside of
+        what's supported, then we snap that
+        value to the floor or ceiling.
+
+        For example, if the next value to generate
+        was between 59 and 69, then if we allowed 
+        values of 59-05, then a value between 0 
+        and 5 would heavily distort the 
+        waveform.
+
+        Args:
+            wave_value (int): Current wave range value to validate
+
+        Returns:
+            int: Same value if within constraints, otherwise 0 or 64 dependant on passing the ceiling/floor
+        """
         if 0 > wave_value:
-            return self.wave_length + wave_value
+            return 0
         elif self.wave_length <= wave_value:
-            return 0 + (wave_value - self.wave_length)
+            return 64
         else:
             return wave_value
