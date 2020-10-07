3aab1f12ad9bd1ed88e0c4cdc100db79dd836749
Jon Herbst <herbtaculargames@gmail.com>
2020-10-07 09:16:54 +0100

    Pin back button to wave generation views to allow the user to navigate back to the main menu and select another expansion chip to manipulate

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ import views.fds_view as fds
 import views.main_menu as main
 import views.n163_view as n163
 
+from functools import partial
+
 class App(tk.Frame):
     """
     Main container to provide a canvas
@@ -42,16 +44,22 @@ class App(tk.Frame):
 
         if 'menu' == view:
             self.master.title('Menu - ' + self.window_title)
+
             self.main = main.MainMenu(self)
             self.main.pack()
+            self.detachBackButton()
         elif 'fds' == view:
             self.master.title('FDS - ' + self.window_title)
+
             self.fds = fds.FDSView(self)
             self.fds.pack()
+            self.attachBackButton()
         elif 'n163' == view:
             self.master.title('N163 - ' + self.window_title)
+
             self.n163 = n163.N163View(self)
             self.n163.pack()
+            self.attachBackButton()
     
     def clearView(self):
         """
@@ -62,6 +70,26 @@ class App(tk.Frame):
         for widget in self.winfo_children():
             widget.destroy()
 
+    def attachBackButton(self):
+        """
+        Create a new, absolutely positioned back button
+        element to the root frame and anchor it to
+        the top-left of the window.
+        """
+        self.menu_back = tk.Button(self.master, padx=10, pady=5, command=partial(self.loadView, 'menu'))
+        self.menu_back['text'] = '<-- Back'
+        self.menu_back.pack()
+        self.menu_back.place(anchor='nw', x=10)
+
+    def detachBackButton(self):
+        """
+        As the back button is attached to the root
+        frame, the element will be destroyed
+        and re-created when necessary.
+        """
+        if hasattr(self, 'menu_back') and None != self.menu_back:
+            self.menu_back.destroy()
+
 if '__main__' == __name__:
     """Setup root tkinter window."""
     root = tk.Tk()
@@ -70,4 +98,3 @@ if '__main__' == __name__:
 
     app = App(master=root)
     app.mainloop()
-    
